4637489ec771e2f511d6e28960d8c19c8779a377
John Davison <unixfreak0037@gmail.com>
2019-12-15 20:29:58 +0000

    Updated cryptography usage.
    - switched to PBKDF2 for AES key generation and validation
      this makes it much harder to brute force guess the password
    - allows user to change encryption password without having to re-encrypt
      everything

--- lib/saq/test_crypto.py
+++ lib/saq/test_crypto.py
@@ -4,28 +4,72 @@ import logging
 
 import saq
 
-from saq.crypto import encrypt_chunk, decrypt_chunk, get_aes_key
+from saq.crypto import *
 from saq.test import *
 
-class ACECryptoTestCase(ACEBasicTestCase):
+class TestCase(ACEBasicTestCase):
 
-    def setUp(self, *args, **kwargs):
-        super().setUp(*args, **kwargs)
-        self.old_password = saq.ENCRYPTION_PASSWORD
-        saq.ENCRYPTION_PASSWORD = get_aes_key('test')
+    #def setUp(self, *args, **kwargs):
+        #super().setUp(*args, **kwargs)
+        #self.old_password = saq.ENCRYPTION_PASSWORD
+        #saq.ENCRYPTION_PASSWORD = get_aes_key('test')
 
-    def tearDown(self, *args, **kwargs):
-        super().setUp(*args, **kwargs)
-        saq.ENCRYPTION_PASSWORD = self.old_password
+    #def tearDown(self, *args, **kwargs):
+        #super().setUp(*args, **kwargs)
+        #saq.ENCRYPTION_PASSWORD = self.old_password
+
+    def test_set_password(self):
+        # ensure a password is not already set
+        self.assertFalse(encryption_key_set())
+        # set the new password
+        set_encryption_password('test')
+        self.assertTrue(encryption_key_set())
+        # verify the password
+        aes_key = get_aes_key('test')
+        self.assertTrue(isinstance(aes_key, bytes))
+        self.assertEquals(len(aes_key), 32)
+
+        # encrypt and decrypt something with this password
+        encrypted_chunk = encrypt_chunk('Hello, World!'.encode(), password=aes_key)
+        self.assertEquals(decrypt_chunk(encrypted_chunk, password=get_aes_key('test')), 'Hello, World!'.encode())
+
+    def test_change_password(self):
+        # make sure that when we change the password we can still decrypt what we encrypted
+        # ensure a password is not already set
+        self.assertFalse(encryption_key_set())
+        # set the new password
+        set_encryption_password('test')
+        self.assertTrue(encryption_key_set())
+        # verify the password
+        aes_key = get_aes_key('test')
+        # now change the password to something else
+        set_encryption_password('new password', old_password='test')
+        # aes key should still be the same
+        self.assertEquals(aes_key, get_aes_key('new password'))
+
+    def test_invalid_password(self):
+        self.assertFalse(encryption_key_set())
+        # set the new password
+        set_encryption_password('test')
+        self.assertTrue(encryption_key_set())
+        with self.assertRaises(InvalidPasswordError):
+            aes_key = get_aes_key('invalid_password')
+
+    def test_password_not_set(self):
+        self.assertFalse(encryption_key_set())
+        with self.assertRaises(PasswordNotSetError):
+            aes_key = get_aes_key('test')
     
-    def test_anp_000_encrypt_chunks(self):
+    def test_encrypt_chunk(self):
+        set_encryption_password('test')
         chunk = b'1234567890'
         encrypted_chunk = encrypt_chunk(chunk)
         self.assertNotEquals(chunk, encrypted_chunk)
         decrypted_chunk = decrypt_chunk(encrypted_chunk)
         self.assertEquals(chunk, decrypted_chunk)
 
-    def test_anp_001_encrypt_empty_chunks(self):
+    def test_encrypt_empty_chunk(self):
+        set_encryption_password('test')
         chunk = b''
         encrypted_chunk = encrypt_chunk(chunk)
         self.assertNotEquals(chunk, encrypted_chunk)
