420ff31720716be524784a66414fa84c7465d85a
John Davison <unixfreak0037@gmail.com>
2020-01-22 13:48:02 +0000

    removed duplicated patch

--- updates/sql/ace/00005.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `encrypted_passwords` (
-  `key` VARCHAR(256) NOT NULL COMMENT 'The name (key) of the value being stored. Can either be a single name, or a section.option key.',
-  `encrypted_value` TEXT NOT NULL COMMENT 'Encrypted value, base64 encoded',
-  PRIMARY KEY (`key`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_520_ci;
