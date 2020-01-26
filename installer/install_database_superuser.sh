986f0800ab7e23b97905109dcba1ff55981e50aa
John Davison <unixfreak0037@gmail.com>
2020-01-26 18:44:52 +0000

    mysql alt root install

--- installer/install_database_superuser.sh
+++ installer/install_database_superuser.sh
@@ -4,13 +4,17 @@
 if [ ! -e sql/create_db_super_user.exec.sql ]
 then
     echo "generating super user mysql account for ACE with random password"
-    tr -cd '[:alnum:]' < /dev/urandom | fold -w14 | head -n1 > .superuser.password
-    echo -ne "user: ace-superuser\npassword: $(cat .superuser.password)\n"
-    # modify the configuration files to use it
-    sed -e 's;^;s/ACE_SUPERUSER_DB_USER_PASSWORD/;' -e 's;$;/g;' .superuser.password > .superuser.password.sed
-    sed -f .superuser.password.sed --follow-symlinks sql/create_db_super_user.sql > sql/create_db_super_user.exec.sql
-    rm .superuser.password.sed
+    password=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w14 | head -n1)
 
+cat > etc/mysql_defaults.root <<EOF
+[client]
+host=localhost
+user=ace-superuser
+password=$password
+EOF
+	chmod 660 etc/mysql_defaults.root
+
+    sed -e "s/ACE_SUPERUSER_DB_USER_PASSWORD/$password/g" sql/create_db_super_user.sql > sql/create_db_super_user.exec.sql
     # create the mysql database user for ace
     sudo mysql < sql/create_db_super_user.exec.sql && rm sql/create_db_super_user.exec.sql
 fi
