7ee04251656bee675746c067b6323499d9fa693a
Holly Fox <Holly.Fox@53.com>
2020-01-28 15:38:54 +0000

    Fix SQL patch

--- updates/sql/ace/00005.sql
+++ updates/sql/ace/00005.sql
@@ -1,12 +1,12 @@
-ALTER TABLE `ace`.`events`
+ALTER TABLE `events`
 ADD COLUMN `event_time` DATETIME DEFAULT NULL;
-ALTER TABLE `ace`.`events`
+ALTER TABLE `events`
 ADD COLUMN `alert_time` DATETIME DEFAULT NULL;
-ALTER TABLE `ace`.`events`
+ALTER TABLE `events`
 ADD COLUMN `ownership_time` DATETIME DEFAULT NULL;
-ALTER TABLE `ace`.`events`
+ALTER TABLE `events`
 ADD COLUMN `disposition_time` DATETIME DEFAULT NULL;
-ALTER TABLE `ace`.`events`
+ALTER TABLE `events`
 ADD COLUMN `contain_time` DATETIME DEFAULT NULL;
-ALTER TABLE `ace`.`events`
+ALTER TABLE `events`
 ADD COLUMN `remediation_time` DATETIME DEFAULT NULL;
