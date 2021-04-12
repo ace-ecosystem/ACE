22291fa1012ccc46a15dc5a182fe228b31d7cf9a
Sean McFeely <mcfeelynaes@gmail.com>
2021-04-12 15:33:53 -0400

    Improved CbResponse guid analyzer

--- saq/modules/process.py
+++ saq/modules/process.py
@@ -13,6 +13,8 @@ from cbapi import auth, connection
 from cbapi.response import *
 from cbapi.errors import ApiError, ObjectNotFoundError
 
+from cbinterface.response.process import process_to_dict, print_process_info
+
 KEY_ASSET_COUNT = 'asset_count'
 KEY_PROCESS_LOGS = 'process_logs'
 
@@ -311,21 +313,23 @@ class ProcessGUIDAnalyzer(AnalysisModule):
         return F_PROCESS_GUID
 
     def execute_analysis(self, observable):
-        try:
-            return self.execute_analysis_wrapper(observable)
-        except ApiError as e:
-            logging.error(f"carbon black API error when analyzing {observable}: {e}")
-            return False
 
-    def execute_analysis_wrapper(self, observable):
-
-        # we only analyze observables that came with the alert and ones with detection points
-        #if not observable in self.root.observables and not observable.is_suspect:
-        #    return False
+        process_id = observable.value
+        process_segment = None
+        if "/" in observable.value:
+            # allow for process_guid/process_segment observable values
+            if not observable.value.count("/") == 1:
+                logging.error(f"process guid/segment format error: {observable}")
+                return False
+            process_id, process_segment = observable.value.split("/")
+            if not re.match("[0-9]{13}", process_segment):
+                logging.error(f"{process_segment} is not in the form of a process segment.")
+                return False
+            process_segment = int(process_segment)
 
         #cbapi does not check for guids and doesn't error correctly
         regex = re.compile('[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}', re.I)
-        if regex.match(observable.value) == None:
+        if regex.match(process_id) == None:
             logging.error("{} is not in the format of a process guid".format(observable.value))
             return False
 
@@ -334,7 +338,11 @@ class ProcessGUIDAnalyzer(AnalysisModule):
         cb = CbResponseAPI(credential_file=saq.CONFIG['carbon_black']['credential_file'])
 
         try:
-            proc = cb.select(Process, observable.value, force_init=True)
+            proc = cb.select(Process, process_id, force_init=True)
+            if process_segment and process_segment not in proc.get_segments():
+                logging.warning(f"segment '{process_segment}' does not exist. Setting to first segment.")
+                process_segment = None
+            proc.current_segment = process_segment
         except ObjectNotFoundError as e:
             logging.error("Could not find process {0:s}".format(observable.value))
             return False
@@ -344,38 +352,19 @@ class ProcessGUIDAnalyzer(AnalysisModule):
         except Exception as e:
             logging.error("Encountered unknown error retrieving process: {0:s}".format(str(e)))
             return False
-
-
-        global ancestry_string
-        ancestry_string = "  === Process Ancestry Walk ===\n"
-        def _ancestry_details(proc, depth):
-            global ancestry_string
-            try:
-                start_time = proc.start or "<unknown>"
-                ancestry_string += "%s%s:  %s %s - %s\n" % ('  '*(depth + 1), start_time, proc.cmdline,
-                                             "(suppressed)" if proc.suppressed_process else "", proc.id)
-            except Exception as e:
-                return
-
-        from cbinterface.modules.process import SuperProcess
-        sp = SuperProcess(proc)
+        
+        if not proc:
+            logging.error(f"this shouldn't happend: couln't get process {proc.id}")
+            return None
 
         analysis = self.create_analysis(observable)
-
-        process_tree = sp.walk_process_tree()
-        process_event_details = sp.events_to_json(segment_limit=segment_limit)
-        process_event_details['process_tree'] =  process_tree.tuple_list()
-        process_event_details['process_tree_str'] = str(process_tree)
-
-        process_event_details['process_info_str'] = str(sp)
-
-        # fill ancestry_string
         try:
-            sp.proc.walk_parents(_ancestry_details)
+            analysis.details = {}
+            # create info string
+            analysis.details['process_info_str'] = print_process_info(proc, return_string=True, raw_print=True)
+            process = process_to_dict(proc, max_segments=1)#segment_limit)
+            analysis.details.update(process)
+            return True
         except Exception as e:
-            logging.error("Error getting ancestry: {}".format(str(e)))
-            ancestry_string += "[ERROR] {}".format(str(e))
-        process_event_details['process_ancestry'] = ancestry_string 
-
-        analysis.details = process_event_details
-        return True
+            logging.error(f"problem exporting cabon black response process: {observable} : {e}")
+            return False
