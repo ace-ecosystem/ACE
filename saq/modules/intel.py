e1a9434ecb306975c71159decb183a7871bb67fd
Sean McFeely <mcfeelynaes@gmail.com>
2021-04-28 14:55:17 -0400

    CBC IOC Analyzer

--- saq/modules/intel.py
+++ saq/modules/intel.py
@@ -5,7 +5,12 @@
 #
 
 import logging
+import pysip
 
+from cbapi.psc import threathunter
+from cbinterface.psc.intel import get_report, is_ioc_ignored
+
+from saq.proxy import proxies
 from saq.analysis import Analysis, DetectionPoint, Observable
 from saq.constants import *
 from saq.database import use_db
@@ -13,7 +18,164 @@ from saq.error import report_exception
 from saq.intel import *
 from saq.modules import AnalysisModule
 
-import pysip
+class CBC_IOC_Analysis(Analysis):
+    """Carbon Black Cloud IOCs"""
+
+    def initialize_details(self):
+        self.details = {}
+
+    @property
+    def jinja_template_path(self):
+        return 'analysis/cbc_intel.html'
+
+    @property
+    def cbc_token(self):
+        return saq.CONFIG['carbon_black']['cbc_token']
+
+    @property
+    def cbc_url(self):
+        return saq.CONFIG['carbon_black']['cbc_url']
+
+    @property
+    def org_key(self):
+        return saq.CONFIG['carbon_black']['org_key']
+
+    @property
+    def ioc_value(self):
+        if not self.details.get('values'):
+            return None
+        value = self.details['values']
+        if len(value) == 1:
+            value = value[0]
+        return value
+
+    @property
+    def report_id(self):
+        if not self.details.get('source_report'):
+            return None
+        return self.details['source_report'].get('id')
+
+    @property
+    def ioc_id(self):
+       return self.details.get('id')
+
+    def get_current_status(self):
+        try:
+            cbapi = threathunter.CbThreatHunterAPI(url=self.cbc_url, token=self.cbc_token, org_key=self.org_key)
+            # HACK: directly setting proxies as passing above reveals cbapi error
+            cbapi.session.proxies = proxies()
+            ignored = is_ioc_ignored(cbapi, self.report_id, self.ioc_id)
+            return "IGNORED" if ignored else "ACTIVE"
+        except Exception as e:
+            logging.error(f"could not get cbc ioc status: {e}")
+            return "Could not get current status."
+
+    def generate_summary(self):
+        if self.details is None:
+            return None
+
+        if 'match_type' not in self.details or \
+           'values' not in self.details:
+            return 'CBC IOC Analysis - ERROR: response is missing fields'
+
+        match_type = self.details['match_type']
+        field = self.details['field'] if self.details.get('field') else None
+
+        summary = f"CBC IOC Analysis - [{match_type}]"
+        if field is not None:
+            summary += f" [{field}]"
+        summary += f" : {self.ioc_value}"
+        return summary
+
+
+class CBC_IOC_Analyzer(AnalysisModule):
+    """Carbon Black Cloud IOC Analyzer."""
+
+    def verify_environment(self):
+        if not 'carbon_black' in saq.CONFIG:
+            raise ValueError("missing config section carbon_black")
+
+        keys = ['cbc_url', 'cbc_token', 'org_key']
+        for key in keys:
+            if key not in saq.CONFIG['carbon_black']:
+                raise ValueError("missing config item {key} in section carbon_black")
+
+    @property
+    def cbc_token(self):
+        return saq.CONFIG['carbon_black']['cbc_token']
+
+    @property
+    def cbc_url(self):
+        return saq.CONFIG['carbon_black']['cbc_url']
+
+    @property
+    def org_key(self):
+        return saq.CONFIG['carbon_black']['org_key']
+
+    @property
+    def generated_analysis_type(self):
+        return CBC_IOC_Analysis
+
+    @property
+    def valid_observable_types(self):
+        return F_INDICATOR
+
+    def execute_analysis(self, indicator):
+
+        # is this a CBC indicator?
+        if not indicator.value.startswith("cbc:"):
+            return False
+
+        # is it of the expected report_id/ioc_id format?
+        if "/" not in indicator.value:
+            logging.error(f"{indicator.value} not correctly formatted as cbc:report_id/ioc_id")
+            return False
+
+        cbapi = threathunter.CbThreatHunterAPI(url=self.cbc_url, token=self.cbc_token, org_key=self.org_key)
+        # HACK: directly setting proxies as passing above reveals cbapi error
+        cbapi.session.proxies = proxies()
+
+        report_id, ioc_id = indicator.value[len('cbc:'):].split('/', 1)
+
+        report = None
+        try:
+            report = get_report(cbapi, report_id)
+        except Exception as e:
+            logging.error(f"error getting report for indicator {indicator.value}: {e}")
+            return False
+
+        if not report:
+            return False
+
+        # does this IOC actually exist in this report?
+        ioc_intel = None
+        for ioc in report["iocs_v2"]:
+            if ioc['id'] == ioc_id:
+                ioc_intel = ioc.copy()
+                break
+        else:
+            logging.error(f"{ioc_id} does not exist in {report_id}")
+            return False
+
+        ioc_intel["ignored"] = "unknown status"
+        try:
+            ioc_intel["ignored"] = is_ioc_ignored(cbapi, report_id, ioc_id)
+        except Exception as e:
+            logging.error(f"error getting indicator status for {indicator.value}: {e}")
+
+        ioc_intel["source_report"] = report
+        del ioc_intel["source_report"]["iocs_v2"]
+        del ioc_intel["source_report"]["iocs"]
+
+        analysis = self.create_analysis(indicator)
+        analysis.details = ioc_intel
+
+        # extract any tags (buckets) associated with the indicator
+        for tag in ioc_intel["source_report"]['tags']:
+            indicator.add_tag(tag)
+
+        return True
+
 
 class IntelAnalysis(Analysis):
     """What are the details of this indicator?"""
