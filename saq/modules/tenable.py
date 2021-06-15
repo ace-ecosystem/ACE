f489abb40212be9e17c4db123fac957e0101d571
Sean McFeely <mcfeelynaes@gmail.com>
2021-06-15 09:39:15 -0400

    TenableAssetSearchAnalyzer

--- saq/modules/tenable.py
+++ saq/modules/tenable.py
@@ -1,5 +1,7 @@
 """Modules related to Tenable."""
 
+# XXX: add result cache ?
+
 import os
 import datetime
 import logging
@@ -13,31 +15,48 @@ from saq.analysis import Analysis, Observable
 from saq.constants import *
 from saq.error import report_exception
 from saq.modules import AnalysisModule
+from saq.proxy import proxies
 
-from saq.tenable import TIO_API
+TIO_API = None
+
+if 'tenable' in saq.CONFIG:
+    access_key = saq.CONFIG['tenable'].get('access_key')
+    secret_key = saq.CONFIG['tenable'].get('secret_key')
+    if access_key and secret_key:
+        try:
+            TIO_API = TenableIO(access_key, secret_key, proxies=proxies())
+        except Exception as e:
+            logging.error(f"couldn't create Tenable.IO API connection: {e}")
+            TIO_API = False
 
 class TenableAssetSearchAnalysis(Analysis):
     """What data does Tenable have on this IPv4?"""
     def initialize_details(self):
         self.details = {}
 
-    #@property
-    #def jinja_template_path(self):
-    #    return "analysis/carbon_black_cloud.html"
+    @property
+    def jinja_template_path(self):
+        return "analysis/custom/tenable.html"
+
+    @property
+    def freshest_result(self):
+        return self.details.get('freshest_result', None)
 
     def generate_summary(self):
         if self.details is None:
             return None
 
-        summary = "Tenable Asset Search"
+        summary = "Tenable Analysis"
 
-        hostname = self.details['freshest_result']['hostname']
-        operating_system = self.details['freshest_result']['operating_system']
+        hostname = self.freshest_result.get('hostname')
+        hostname = hostname[0] if hostname and len(hostname) == 1 else hostname
+        operating_system = self.freshest_result.get('operating_system')
+        operating_system = operating_system[0] if operating_system and len(operating_system) == 1 else operating_system
 
         if len(self.details['all_results']) > 1:
-            summary += f" - Most Recent: {hostname} ({operating_system})"
+            summary += f" - Most Recent Result: {hostname} - {operating_system}"
         else:
-            summary += f": {hostname} ({operating_system})"
+            summary += f": {hostname} - {operating_system}"
 
         return summary
 
@@ -56,21 +75,17 @@ class TenableAssetSearchAnalyzer(AnalysisModule):
         return F_IPV4
 
     def custom_requirement(self, observable):
-        if observable not in self.root.observables and not observable.is_suspect:
-            # we only analyze observables that came with the alert and ones with detection points
-            logging.debug(f"{self} skipping {observable} because it's not a root observable and it has no detection points.")
-            return False
-        # NOTE: would searching for all ipv4 work for asset ISP addresses they're seen from? XXX prob not often enough to justify.
         if observable.type == F_IPV4 and not observable.is_managed():
             # we only analyze our own IP address space.
-            logging.info(f"{self} skipping Tenable.IO analysis for non-managed ipv4 {observable}")
+            logging.info(f"{self} skipping Tenable.IO analysis for non-managed or private ipv4 {observable}")
             return False
+        #if observable not in self.root.observables:
+        #    logging.debug(f"{self} skipping {observable} because it didn't come with the alert.")
+        #    return False
         return True
 
-
     def execute_analysis(self, observable):
-        
-        # TODO: cache results for X time
+
         try:
             result = TIO_API.workbenches.assets(('ipv4','eq',observable.value))
         except Exception as e:
@@ -89,7 +104,7 @@ class TenableAssetSearchAnalyzer(AnalysisModule):
         analysis.details = {'freshest_result': result[-1],
                             'all_results': result}
 
-        # get the most recent result
+        # Get the most recent result, don't assume it will always be the one on the end of the result list.
         for asset_result in result:
             last_seen_time = dateutil.parser.parse(asset_result["last_seen"])
             if last_seen_time > dateutil.parser.parse(analysis.details['freshest_result']['last_seen']):
